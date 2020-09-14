d40c5543a27689c918518078c3f5889669ee0a24
jav <vojirja@gmail.com>
2020-09-14 16:47:57 +0200

    Instructions page

--- src/routes/public/instructions/index.tsx
+++ src/routes/public/instructions/index.tsx
@@ -2,6 +2,7 @@ import {Component, h} from "preact";
 import * as style from "./style.css";
 import InstructionsMenu from "./menu";
 import Navigation from "../../../components/navigation";
+import Hider from "../../../components/hider";
 
 export default class Instructions extends Component<any, any> {
     constructor() {
@@ -20,128 +21,69 @@ export default class Instructions extends Component<any, any> {
     };
 
     render () {
+        var basic = 'This tool is used to modify the supplied content by the user, who himself sets the specific medium and the basic parameters of the resulting modification✨\n' +
+            '\n' +
+            'The application is divided into three categories:\n' +
+            '1. Modules (`Word` and `Picture` modules are available at the moment)\n' +
+            '2. Settings (currently for `Medium` and `Print`)\n' +
+            '3. Instractions (where you are currently located)\n' +
+            '\n' +
+            'Before sending data:\n' +
+            '1. Add or Delete folders in Modules (one folder is the minimum)*\n' +
+            '2. Upload your contents to folders\n' +
+            '3. Fill the basic parametres in Settings\n' +
+            '4. Send the contents using the “Send button” to the tool\n' +
+            '\n' +
+            'After sending the data:\n' +
+            '1. download the result of the tool\n' +
+            '2. use it as you see fit**\n' +
+            '\n' +
+            '* The type, number, order of Folders and Text formatting have an effect on the final editing of the content\n' +
+            '** for now only the running medium “Tshirt”  is delivered to ensure production in any local Copy Center / Print Studio by your own\n' +
+            '\n' +
+            'Have fun 🎮';
+
+        const content = 'Word module:\n' +
+            '1. in the `Word` folder you can write text in a wide range of languages ​​or upload links (these are displayed as QR codes in the result)\n' +
+            '2. the tool respects text formatting\n' +
+            '3. currently only text division using the “Enter” key is available)\n' +
+            '\n' +
+            'Picture module:\n' +
+            '1. You can upload JPEG, PNG, PDF files to the `Picture` folder. and TIFF.\n' +
+            '2. maximum file size ??? MB\n' +
+            '3. the recommended image resolution is 300 dpi\n' +
+            '4. if the resolution is lower, the tool will automatically use the program to improve the image resolution\n' +
+            '    \n' +
+            'Curator mode:\n' +
+            '1. the maximum number of characters in the Word folder is ??? characters\n' +
+            '2. the maximum number of files in the Picture folder is ??? files\n' +
+            '3. after exceeding these limits, the tool automatically starts the curator mode, which randomly selects as many words / files as possible from the uploaded content, which it can edit respects';
         return (
 
             <div>
                 <Navigation> </Navigation>
                 <div>
 
-                    <div class={style.main}>
 
-                        <div class={style.scrollable}>
-                            <p>BASIC INSTRUCTIONS</p>
-
-                            <p>
-                            1. Once registered, you will have access to a personal dashboard to manage subscriptions and preferences.
-                            </p>
-                            <p>
-                            2. You will automatically be charged each month for each active module.
-                            </p>
-                            <p>
-                            3. You may unsubscribe from a module at any time –– just toggle the option off and update your OS‌.
-                            </p>
-                            <p>
-                            4. For digital-only modules, once a payment is made you will have immediate 30-day access – no refunds. If you have any questions or issues, feel free to reach out to support@ya.c.ya.g.com.
-                            </p>
-
-                            <p> 1) Neither the Font Software nor any of its individual components, in Original or Modified Versions, may be sold by itself.</p>
-
-                            <p>
-                            2) Original or Modified Versions of the Font Software may be bundled,
-                            redistributed and/or sold with any software, provided that each copy
-                            contains the above copyright notice and this license. These can be
-                            included either as stand-alone text files, human-readable headers or
-                            in the appropriate machine-readable metadata fields within text or
-                            binary files as long as those fields can be easily viewed by the user.
-                            </p>
-
-                            <p>
-                            3) No Modified Version of the Font Software may use the Reserved Font
-                            Name(s) unless explicit written permission is granted by the corresponding
-                            Copyright Holder. This restriction only applies to the primary font name as
-                            presented to the users.
-                            </p>
-
-                            <p>
-                            4) The name(s) of the Copyright Holder(s) or the Author(s) of the Font
-                            Software shall not be used to promote, endorse or advertise any
-                            Modified Version, except to acknowledge the contribution(s) of the
-                            Copyright Holder(s) and the Author(s) or with their explicit written
-                            permission.
-                            </p>
-
-                            <p>
-                            5) The Font Software, modified or unmodified, in part or in whole,
-                            must be distributed entirely under this license, and must not be
-                            distributed under any other license. The requirement for fonts to
-                            remain under this license does not apply to any document created
-                            using the Font Software
-                            </p>
-
-
+                    <Hider headline='Basic instrucitons'>
+                        <div class={style.textBlock}>
+                            {basic}
                         </div>
-                        <div class={style.scrollable}>
+                    </Hider>
 
-                            <p>PERMISSION & CONDITIONS</p>
-
-                            <p>
-                            Permission is subject to the following conditions:
-                            </p>
-
-                            <div class={style.license}>
-                                <p>
-                                    1) Neither the Font Software nor any of its individual components,
-                                    in Original or Modified Versions, may be sold by itself.
-                                </p>
-
-                                <p>
-                                    2) Original or Modified Versions of the Font Software may be bundled, redistributed and/or sold with any software, provided that each copy contains the above copyright notice and this license. These can be included either as stand-alone text files, human-readable headers or
-                                    in the appropriate machine-readable metadata fields within text or
-                                    binary files as long as those fields can be easily viewed by the user.
-                                </p>
-
-                                <p>
-                                    3) No Modified Version of the Font Software may use the Reserved Font
-                                    Name(s) unless explicit written permission is granted by the corresponding
-                                    Copyright Holder. This restriction only applies to the primary font name as
-                                    presented to the users.
-                                </p>
-
-                                <p>
-                                    4) The name(s) of the Copyright Holder(s) or the Author(s) of the Font
-                                    Software shall not be used to promote, endorse or advertise any
-                                    Modified Version, except to acknowledge the contribution(s) of the
-                                    Copyright Holder(s) and the Author(s) or with their explicit written
-                                    permission.
-                                </p>
-
-                                <p>
-                                    5) The Font Software, modified or unmodified, in part or in whole,
-                                    must be distributed entirely under this license, and must not be
-                                    distributed under any other license. The requirement for fonts to
-                                    remain under this license does not apply to any document created
-                                    using the Font Software.
-                                </p>
-                            </div>
-
-
-
-
-
-
-
-                            <p>
-                            Copyright Holder(s) or the Author(s) of the Font Software shall not be used to promote, endorse or advertise any Modified Version, except to acknowledge the contribution(s) of the
-                            </p>
-                            <p>
-                            Copyright Holder(s) and the Author(s) or with their explicit written
-                            permission.
-                            </p>
-
-
-
+                    <Hider headline='Content format support'>
+                        <div class={style.textBlock}>
+                            {content}
                         </div>
-                    </div>
+                    </Hider>
+
+                    <Hider headline='Methods'>
+                    </Hider>
+
+                    <Hider headline='Archive'>
+                    </Hider>
+
+
                     <InstructionsMenu></InstructionsMenu>
                 </div>
             </div>
--- src/routes/public/instructions/menu.tsx
+++ src/routes/public/instructions/menu.tsx
@@ -1,7 +1,6 @@
 import {Component, FunctionalComponent, h} from "preact";
-import { Link } from "preact-router/match";
 import * as style from "../../../components/menu/menuStyle.css";
-import {route} from "preact-router";
+
 
 
 
@@ -11,12 +10,13 @@ export default class InstructionsMenu extends Component<any, any> {
     render(props?: preact.RenderableProps<any>, state?: Readonly<any>, context?: any): preact.ComponentChild {
             return (
                 <div class={style.main}>
-                    <div class={style.a}>
-                        <button type="submit" class={style.menuButton} onClick={()=>{route("/")}}>Back to menu</button>
+                    <div class={style.a} style={{fontSize: '15px', textDecoration: 'underline'}}>
+                        Email us (ask me anything) <br />
+                        Log out (see you soon)
                     </div>
 
                     <div class={style.c}>
-                        <button type="submit" class={style.menuButton + " grayButton"}>I have read and agree</button>
+
                     </div>
                 </div>
             );
